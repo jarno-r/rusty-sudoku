4b74c1d01c18fd02b3608467fdc0c04a4f9cd22b
Jarno Rajala <jarno.rajala@schibsted.com>
2024-03-19 15:36:18 +0200

    pretty

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::cmp::max;
+use core::fmt;
+use std::cmp::{max, min};
 use std::fmt::{Display, Formatter};
 use std::io::prelude::*;
+use std::iter::repeat;
 use std::{
     fs::File,
     io::{self, BufReader, Error},
@@ -19,8 +21,11 @@ struct Args {
 }
 
 struct Sudoku {
+    name: String,
     width: usize,
     height: usize,
+    box_width: usize,
+    box_height: usize,
     grid: Vec<u8>,
 }
 
@@ -47,13 +52,13 @@ impl Sudoku {
 
         let mut max_width = 0;
         let mut height = 0;
-        let mut raw_lines = vec![vec![0u8; 0];0];
+        let mut raw_lines = vec![vec![0u8; 0]; 0];
         for (i, line) in f.lines().enumerate() {
             let line = line?;
             let raw = line;
 
             // Do not count empty lines at the end of file.
-            if !raw.is_empty() {
+            if !raw.trim().is_empty() {
                 height = i + 1;
             }
 
@@ -70,21 +75,39 @@ impl Sudoku {
         }
 
         let width = max_width;
+        // Extend potentially missing rows.
+        if height < width {
+            height = width;
+        }
         let mut grid = vec![0u8; width * height];
 
-        for i in 0..height {
-            let mut p = 0;
+        for i in 0..min(height, raw_lines.len()) {
             for j in 0..width {
                 if raw_lines[i].len() > j {
                     grid[i * width + j] = raw_lines[i][j];
                 }
-                p+=1;
             }
         }
 
+        if width != height {
+            Err(io::Error::other(
+                "Width and height of the grid do not match.",
+            ))?;
+        }
+
+        if width != 9 && width != 16 {
+            Err(io::Error::other("Only 9x9 and 16x16 puzzles supported."))?;
+        }
+
+        let box_width = if width == 9 { 3 } else { 4 };
+        let box_height = box_width;
+
         Ok(Sudoku {
+            name: file.to_string(),
             width,
             height,
+            box_width,
+            box_height,
             grid,
         })
     }
@@ -103,15 +126,32 @@ impl Display for Sudoku {
             return b as char;
         }
 
-        write!(f,"{} by {}",self.width,self.height)?;
-        for i in 0..self.height {
-            write!(f, "\n|")?;
-            let line:String = self.grid[i * self.width..(i + 1) * self.width]
-                .iter()
-                .map(|c| to_char(*c))
-                .collect();
-            write!(f, "{}", line)?;
-            write!(f, "|")?;
+        write!(f, "{}, {} by {}:", self.name, self.width, self.height)?;
+
+        let write_spacer = |f: &mut Formatter| {
+            write!(f, "\n+")?;
+            for _ in 0..self.width / self.box_width {
+                write!(f, "{}", "--".repeat(self.box_width))?;
+                write!(f, "+")?;
+            }
+            Ok(()) as fmt::Result
+        };
+
+        write_spacer(f)?;
+        for i in 0..self.height / self.box_height {
+            for j in 0..self.box_height {
+                write!(f, "\n|")?;
+                for k in 0..self.width / self.box_width {
+                    let base = (i * self.box_height + j) * self.box_width + self.box_width * k;
+                    let seg: String = self.grid[base..base + self.box_width]
+                        .iter()
+                        .map(|c| to_char(*c))
+                        .flat_map(|c| [c,' '])
+                        .collect();
+                    write!(f, "{}|", seg)?;
+                }
+            }
+            write_spacer(f)?;
         }
 
         Ok(())
@@ -121,9 +161,9 @@ impl Display for Sudoku {
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    println!("Loading {}",&args.puzzle);
+    println!("Loading {}", &args.puzzle);
     let sudoku = Sudoku::read_from_file(&args.puzzle)?;
-    println!("{}",sudoku);
+    println!("{}", sudoku);
 
     Ok(())
 }
